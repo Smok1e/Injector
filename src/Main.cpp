08d3ebd2249735b55bd6c9c977a22a6f94c5345a
smok1e <fedotcher@gmail.com>
2025-03-09 16:29:33 +0300

    Done

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,32 +6,70 @@
 
 #include <Windows.h>
 #include <Psapi.h>
+#include <Shlwapi.h>
 
 #include <ArgParser/ArgParser.hpp>
 
 //========================================
 
-bool QUIET = false;
+class WinAPIError: public std::runtime_error
+{
+public:
+	using std::runtime_error::runtime_error;
+
+};
 
 //========================================
 
-void Inject(HANDLE process, std::string_view dll_path);
+class Main
+{
+public:
+	Main() = default;
+	~Main();
+
+	void start(int argc, char* argv[]);
+
+private:
+	ArgParser m_args {
+	 	{"help",   "Print usage reference and exit"    },
+	 	{"name",   "Identify process by name substring"},
+	 	{"create", "Create process injected"           },
+		{"quiet",  "Suppress output"                   }
+	};
+
+	bool        m_quiet          = false;
+	HANDLE      m_process_handle = nullptr;
+	HANDLE      m_thread_handle  = nullptr;
+	std::string m_dll_name       = "";
+
+	void inject();
+
+};
 
 //========================================
 
-int main(int argc, char* argv[])
+HANDLE FindProcess(std::string_view substr);
+
+//========================================
+
+Main::~Main()
+{
+	if (m_thread_handle)
+	{
+		ResumeThread(m_thread_handle);
+		CloseHandle(m_thread_handle);
+	}
+
+	if (m_process_handle)
+		CloseHandle(m_process_handle);
+}
+
+void Main::start(int argc, char* argv[])
 {
 	try
 	{
-		ArgParser args {
-	 		{"help",   "Print usage reference and exit"    },
-	 		{"name",   "Identify process by name substring"},
-	 		{"create", "Create process injected"           },
-			{"quiet",  "Suppress output"                   }
-		};
-	 
-		args.parse(argc, argv);
-		if (args["help"] || args.getArgumentCount() < 2)
+		m_args.parse(argc, argv);
+		if (m_args["help"] || m_args.getArgumentCount() < 2)
 		{
 			std::cout << std::format(
 				"Usage: {0}    [OPTIONS] <PID>  <DLL> - inject DLL into an existing process by PID \n"
@@ -39,64 +77,115 @@ int main(int argc, char* argv[])
 				"       {0} -c [OPTIONS] <EXE>  <DLL> - create process with DLL injected           \n"
 				"                                                                                  \n"
 				"Available options:                                                                \n",
-				args.getExecutablePath().filename().string()
+				m_args.getExecutablePath().filename().string()
 			);
 
-			std::cout << args << std::endl;
+			std::cout << m_args << std::endl;
+			return;
 		}
 
-		QUIET = args["quiet"];
+		m_quiet = m_args["quiet"];
+		m_dll_name = m_args[1].as<std::string_view>();
 
-		std::filesystem::path dll_path = args[1];
+		// Create child process
+		if (m_args["create"])
+		{
+			std::string exe_path = m_args[0];
 
-		HANDLE process = reinterpret_cast<HANDLE>(args[0].as<unsigned long long>());
-		Inject(process, dll_path.string());
+			std::string command_line = exe_path;
+			for (const auto& argument: m_args.getRemainingArguments())
+				(command_line += ' ') += argument;
+
+			STARTUPINFOA        startup_info = {};
+			PROCESS_INFORMATION process_info = {};
+
+			if (
+				!CreateProcessA(
+					exe_path.c_str(),
+					command_line.data(),
+					NULL,
+					NULL,
+					false,
+					CREATE_SUSPENDED,
+					NULL,
+					NULL,
+					&startup_info,
+					&process_info
+				)
+			)
+				throw WinAPIError("Unable to create process");
+
+			m_process_handle = process_info.hProcess;
+			m_thread_handle  = process_info.hThread;
+
+			inject();
+		}
+
+		// Find existing process
+		else
+		{
+			m_process_handle = m_args["name"]
+				? FindProcess(m_args[0])
+				: OpenProcess(PROCESS_ALL_ACCESS, true, m_args[0]);
+
+			if (!m_process_handle)
+				throw std::runtime_error("Process not found");
+
+			inject();
+		}
 	}
-	
-	catch (std::exception exc)
+
+	catch (const WinAPIError& exc)
+	{
+		char* message = nullptr;
+		FormatMessageA(
+			FORMAT_MESSAGE_FROM_SYSTEM     | 
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			nullptr,
+			GetLastError(),
+			0,
+			reinterpret_cast<char*>(&message),
+			0,
+			nullptr
+		);
+
+		std::cerr << exc.what() << ": " << message << std::endl;
+		LocalFree(message);
+	}
+
+	catch (const std::exception& exc)
 	{
 		std::cerr << exc.what() << std::endl;
 	}
 }
 
-//========================================
-
-void Inject(HANDLE process, std::string_view dll_path)
+void Main::inject()
 {
 	auto* remote_dll_path = reinterpret_cast<char*>(
 		VirtualAllocEx(
-			process,
+			m_process_handle,
 			nullptr,
-			dll_path.length() + 1,
+			m_dll_name.length() + 1,
 			MEM_COMMIT,
 			PAGE_READWRITE
 		)
 	);
 
 	if (!remote_dll_path)
-		throw std::runtime_error("Unable to remotely allocate memory in target process");
+		throw WinAPIError("Unable to remotely allocate memory in target process");
 
 	if (!WriteProcessMemory(
-		process,
+		m_process_handle,
 		remote_dll_path,
-		dll_path.data(),
-		dll_path.length(),
+		m_dll_name.data(),
+		m_dll_name.length() + 1,
 		nullptr
 	))
-		throw std::runtime_error("Unable to write dll path into target process memory");
-
-	char trailing_null = '\0';
-	if (!WriteProcessMemory(
-		process,
-		remote_dll_path + dll_path.length(),
-		&trailing_null,
-		sizeof(trailing_null),
-		nullptr
-	))
-		throw std::runtime_error("Unable to write trailing null into target process memory");
+		throw WinAPIError("Unable to write dll path into target process memory");
 
 	auto remote_thread = CreateRemoteThread(
-		process,
+		m_process_handle,
 		nullptr,
 		0,
 		reinterpret_cast<LPTHREAD_START_ROUTINE>(LoadLibraryA),
@@ -106,18 +195,67 @@ void Inject(HANDLE process, std::string_view dll_path)
 	);
 
 	if (!remote_thread)
-		throw std::runtime_error("Unable to start remote thread");
+		throw WinAPIError("Unable to start remote thread");
 
 	WaitForSingleObject(remote_thread, INFINITE);
+	VirtualFreeEx(m_process_handle, remote_dll_path, 0, MEM_RELEASE);
 
-	if (!QUIET)
+	if (!m_quiet)
 	{
 		char process_name[MAX_PATH] = "";
-		GetModuleBaseNameA(process, 0, process_name, std::size(process_name));
+		GetModuleBaseNameA(m_process_handle, 0, process_name, std::size(process_name));
 
 		std::cout 
-			<< dll_path << " was successfully injected into " << process_name << std::endl;
+			<< m_dll_name 
+			<< " was successfully injected into " << process_name 
+			<< std::endl;
 	}
+}
+
+//========================================
+
+HANDLE FindProcess(std::string_view substr)
+{
+	DWORD process_ids[1024] = {};
+	DWORD read_bytes = 0;
+	EnumProcesses(process_ids, sizeof(process_ids), &read_bytes);
+
+	for (size_t i = 0; i < read_bytes/sizeof(DWORD); i++)
+	{
+		HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, false, process_ids[i]);
+
+		char buffer[MAX_PATH] = "";
+		std::string_view process_name(
+			buffer,
+			GetModuleBaseNameA(process, 0, buffer, std::size(buffer))
+		);
+		
+		if (
+			std::search(
+				process_name.begin(), 
+				process_name.end(),
+				substr.begin(), 
+				substr.end(),
+				[](char a, char b) -> bool
+				{
+					return std::tolower(a) == std::tolower(b);
+				}
+			) != process_name.end()
+		)
+			return process;
+
+		CloseHandle(process);
+	}
+
+	return nullptr;
+}
+
+//========================================
+
+int main(int argc, char* argv[])
+{
+	Main instance;
+	instance.start(argc, argv);
 }
 
 //========================================
